6c767dc27ba547df79d07f5f1c29506b4b1115c5
Yu, Zijun <zijun.yu@intel.com>
2026-06-15 15:18:06 +0800

    Fix after rebase

--- ggml/src/ggml-openvino/ggml-openvino.cpp
+++ ggml/src/ggml-openvino/ggml-openvino.cpp
@@ -1165,7 +1165,7 @@ static bool is_op_unsupported_case(const ggml_tensor * op) {
             return true;
         }
         // K > 1 (multiple state snapshots) not supported by fused op
-        if (op->src[5]->ne[1] > 1) {
+        if (op->src[5]->ne[3] > 1) {
             return true;
         }
         break;

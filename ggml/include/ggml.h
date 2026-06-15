107fe69fd253c0fd3906fa5cc16af4959f6cc60e
Yu, Zijun <zijun.yu@intel.com>
2026-06-15 15:35:55 +0800

    Revert "OpenVINO backend: 1) ensure unique node names for OpenVINO; 2) add org_src to recorde the src ggml tensor for OpenVINO dynamic shape infer"
    This reverts commit ee94aabd6dc5a6b322527682928bf3925a54c625.

--- ggml/include/ggml.h
+++ ggml/include/ggml.h
@@ -695,9 +695,7 @@ extern "C" {
 
         void * extra; // extra things e.g. for ggml-cuda.cu
 
-        char padding[16];
-        // add a struct ggml_tensor * named org_src, initialized to NULL, for keeping track of original source tensors in case of in-place operations
-        struct ggml_tensor * org_src;
+        char padding[8];
     };
 
     static const size_t GGML_TENSOR_SIZE = sizeof(struct ggml_tensor);
